be06207dea84838f26fb2db0c67f802cc754fa80
Vadim <vadimirtoej@gmail.com>
2014-10-24 01:03:09 +0400

    my first commit

---
+++ simple_project/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter x1: " ;
+    double x1;
+    cin >> x1;
+    cout << "Enter x2: ";
+    double x2;
+    cin >> x2;
+    return 0;
+}
